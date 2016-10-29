57c8b5b0261494fc42debbc0c79776dd6e128d53
Matthias J. Kastner <mail@matthias-kastner.de>
2016-10-29 14:27:04 +0200

    fix for app.ino
    app.ino still had some dependencies on the original program.
    as we have no pressure readout I removed it.
    Also the standard deviation is not important for ModBus-Readouts.
    Originally it was designed for analog readout.

--- app/app.ino
+++ app/app.ino
@@ -13,65 +13,39 @@ MKUserInput Input;
 #include <MKMath.h>
 MKMath Math;
 
-long runtime=0;
-
 void setup()
 {
-  //INIT
   Serial.begin(9600);
   Eurotherm2416.begin(9600);
   Input.setInput(Serial);
 }
 
 void loop()
-{ 
+{
   checkinput();
   Serial.print((float)millis()/1000.0);
-  Temperature.batch(Math,2); //Temperature change is very low and Modbus protocol is slow
-  printTemperature(); 
-  Serial.println();
-  secure();
-}
-
-void secure()
-{
-  if (Pressure.getValue()>980.0)
-  { 
-      Argon.set(0);
-      Ethanol.set(0);
-      //Pressure.set(1000);
-  }
-    
+  Temperature.batch(Math, 2); // readout Temperature 2 times and average.
+  printTemperature();
 }
 
 void printTemperature()
 {
-  Serial.print("\tTemp\t");
+  Serial.print("\tTemperature\t");
   Serial.print(Temperature.getTemperature());
-  Serial.print("\t");
-  Serial.print(Temperature.getTDeviation());
-  Serial.print("\t");
-  Serial.print(Temperature.getError());  
-  Serial.print("\t");
+  Serial.print("\tSetPoint\t");
   Serial.print(Temperature.getSetPoint());
-  
-  Serial.print("\tPower\t");  
+  Serial.print("\tPower\t");
   Serial.print(Temperature.getPower());
-/*  Serial.print("\t");
-  Serial.print(Temperature.getPDeviation());
-  Serial.print("\t");
-  Serial.print(Temperature.getError());
-  Serial.print("\t");
-  Serial.print(Temperature.getSetPointPower());  */
+  Serial.println();
 }
 
 void checkinput()
 {
-  Input.waitfor('t','/');  
+  Input.waitfor('t','/');
     if (Input.ready()) { Temperature.set(Input.receive()); }
   Input.waitfor('d',',');
-    if (Input.ready()) 
-    { 
+    if (Input.ready())
+    {
       word value = Input.receive();
       switch (value) {
       case 1:
@@ -79,15 +53,15 @@ void checkinput()
       break;
       case 2:
         Input.debug(Serial);
-      break;      
+      break;
       case 3:
         Math.debug(Serial);
       break;
       case 4:
         Modbus.debug(Serial);
-      break;      
+      break;
       }
-    } 
-  Input.clear();  
+    }
+  Input.clear();
 }
 
